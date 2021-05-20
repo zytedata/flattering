a1323bfc390c7e58396e8761595c957005c1fb87
sortafreel <woutut@gmail.com>
2021-05-20 15:28:35 +0300

    Add base export from product to CSV.

--- autocrawl/utils/csv_export.py
+++ autocrawl/utils/csv_export.py
@@ -5,37 +5,31 @@ from scalpl import Cut
 
 
 class CSVExporter:
-    def __init__(self):
+    def __init__(self, named_properties, skip_fields, schema):
         # Insertion-ordered dict
-        self.csv_schema = {}
-        self.skip_fields = {"probability", "_key"}
-        with open("autocrawl/utils/csv_export_assets/products_full_test.json") as f:
-            self.product_list = json.loads(f.read())
-        with open("autocrawl/utils/csv_export_assets/product_full_schema.json") as f:
-            self.product_schema = Cut(json.loads(f.read()))
-        self.named_properties = {
-            "gtin": "type",
-            "additionalProperty": "name",
-            "ratingHistogram": "ratingOption"
-        }
+        self.headers_meta = {}
+        self.flat_headers = []
+        self.skip_fields = skip_fields
+        self.named_properties = named_properties
+        self.schema = schema
 
     def process_object(self, prefix, object_value, object_schema):
         for property_name in object_value:
             property_path = f"{prefix}.{property_name}"
             property_type = object_schema["properties"][property_name]["type"]
             if property_type not in {"object", "array"}:
-                if self.csv_schema.get(property_path) is None:
-                    self.csv_schema[property_path] = {"count": 1}
+                if self.headers_meta.get(property_path) is None:
+                    self.headers_meta[property_path] = {"count": 1}
             elif property_type == "object":
                 self.process_object(property_path, object_value[property_name])
 
     def process_array(self, prefix, array_value, array_schema):
-        if self.csv_schema.get(prefix) is None:
-            self.csv_schema[prefix] = {"count": 0, "properties": []}
+        if self.headers_meta.get(prefix) is None:
+            self.headers_meta[prefix] = {"count": 0, "properties": []}
         # Currently there're no array of arrays, so arrays could be ignored
         if array_schema["items"]["type"] not in {"object"}:
-            if self.csv_schema[prefix]["count"] < len(array_value):
-                self.csv_schema[prefix]["count"] = len(array_value)
+            if self.headers_meta[prefix]["count"] < len(array_value):
+                self.headers_meta[prefix]["count"] = len(array_value)
         else:
             # TODO Check if object properties are not nested objects
             # Process only the first offer
@@ -44,19 +38,19 @@ class CSVExporter:
             if prefix in self.named_properties:
                 for element in array_value:
                     property_path = f"{prefix}.{element[self.named_properties[prefix]]}"
-                    if property_path in self.csv_schema:
+                    if property_path in self.headers_meta:
                         continue
                     property_schema = array_schema["items"]["properties"][self.named_properties[prefix]]
                     property_type = property_schema["type"]
                     if property_type not in {"object", "array"}:
-                        self.csv_schema[property_path] = {"count": 1}
+                        self.headers_meta[property_path] = {"count": 1}
                     elif property_type == "array":
                         self.process_array(property_path, element, property_schema)
                     else:
                         self.process_object(property_path, element, property_schema)
             else:
-                if self.csv_schema[prefix]["count"] < len(array_value):
-                    self.csv_schema[prefix]["count"] = len(array_value)
+                if self.headers_meta[prefix]["count"] < len(array_value):
+                    self.headers_meta[prefix]["count"] = len(array_value)
                 # Checking manually to keep properties order instead of checking subsets
                 for element in array_value:
                     for property_name, property_value in element.items():
@@ -66,38 +60,36 @@ class CSVExporter:
                         ]
                         property_type = property_schema["type"]
                         if property_type not in {"object", "array"}:
-                            if property_name in self.csv_schema[prefix]["properties"]:
+                            if property_name in self.headers_meta[prefix]["properties"]:
                                 continue
-                            self.csv_schema[prefix]["properties"].append(property_name)
+                            self.headers_meta[prefix]["properties"].append(property_name)
                         elif property_type == "array":
                             self.process_array(property_path, property_value, property_schema)
                         else:
                             self.process_object(property_path, property_value, property_schema)
 
-    def process_product_list(self):
-        for product in self.product_list:
-            for product_field, product_value in product.items():
-                if product_field in self.csv_schema:
-                    continue
-                elif product_field in self.skip_fields:
-                    self.csv_schema[product_field] = {"count": 1}
-                    continue
-                field_schema = self.product_schema.get(
-                    f"allOf[0].properties.{product_field}"
-                )
-                # Save non-array/object fields
-                if field_schema["type"] not in {"object", "array"}:
-                    self.csv_schema[product_field] = {"count": 1}
-                    continue
-                elif field_schema["type"] == "object":
-                    self.process_object(product_field, product_value, field_schema)
-                else:
-                    self.process_array(product_field, product_value, field_schema)
-        print(json.dumps(self.csv_schema, indent=4))
+    def process_product(self, product):
+        for product_field, product_value in product.items():
+            if product_field in self.headers_meta:
+                continue
+            elif product_field in self.skip_fields:
+                self.headers_meta[product_field] = {"count": 1}
+                continue
+            field_schema = self.schema.get(
+                f"allOf[0].properties.{product_field}"
+            )
+            # Save non-array/object fields
+            if field_schema["type"] not in {"object", "array"}:
+                self.headers_meta[product_field] = {"count": 1}
+                continue
+            elif field_schema["type"] == "object":
+                self.process_object(product_field, product_value, field_schema)
+            else:
+                self.process_array(product_field, product_value, field_schema)
 
-    def flatten_csv_schema(self):
+    def flatten_headers(self):
         headers = []
-        for field, meta in self.csv_schema.items():
+        for field, meta in self.headers_meta.items():
             if meta["count"] == 0:
                 continue
             elif not meta.get("properties"):
@@ -110,12 +102,47 @@ class CSVExporter:
                 for i in range(meta["count"]):
                     for pr in meta["properties"]:
                         headers.append(f"{field}[{i}].{pr}")
-        return headers
+        self.flat_headers = headers
+
+    def export_product(self, product):
+        row = []
+        product_data = Cut(product)
+        for header in self.flat_headers:
+            header_path = header.split(".")
+            if header_path[0] not in self.named_properties:
+                row.append(product_data.get(header, ""))
+            else:
+                for pr in product_data[header_path[0]]:
+                    if pr.get(self.named_properties[header_path[0]]) == header_path[1]:
+                        # TODO Require not only name of the name property, but also value property
+                        row.append(str(pr))
+        pprint(row)
 
 
-waka = CSVExporter()
-waka.process_product_list()
-print('*' * 500)
+with open("autocrawl/utils/csv_export_assets/products_xod_test.json") as f:
+    product_list = json.loads(f.read())
+with open("autocrawl/utils/csv_export_assets/product_xod_schema.json") as f:
+    product_schema = Cut(json.loads(f.read()))
+test_named_properties = {
+    "gtin": "type",
+    "additionalProperty": "name",
+    "ratingHistogram": "ratingOption"
+}
+# TODO Pick all other properties from the objects as an additional columns
+# isbn.value, ratingHistogram.5 stars.ratingCount, ratingHistogram.5 stars.ratingPercentage etc.
+test_skip_fields = {"probability", "_key"}
+
+csv_exporter = CSVExporter(test_named_properties, test_skip_fields, product_schema)
+for p in product_list:
+    csv_exporter.process_product(p)
+
 from pprint import pprint
-
-pprint(waka.flatten_csv_schema())
+print('*' * 500)
+pprint(csv_exporter.headers_meta)
+print('*' * 500)
+csv_exporter.flatten_headers()
+pprint(csv_exporter.flat_headers)
+print('*' * 500)
+for p in product_list:
+    csv_exporter.export_product(p)
+    break
