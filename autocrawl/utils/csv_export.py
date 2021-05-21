c4927c800b308a08c0a78a7d9361fbc603d8a4f0
sortafreel <woutut@gmail.com>
2021-05-21 17:41:45 +0300

    Organize code and rename variables for better clarity.

--- autocrawl/utils/csv_export.py
+++ autocrawl/utils/csv_export.py
@@ -1,6 +1,7 @@
 import json
-from collections import defaultdict
-
+import csv
+import os
+from pprint import pprint
 from scalpl import Cut
 
 
@@ -43,18 +44,17 @@ class CSVExporter:
                     value_properties = [x for x in element.keys() if x != self.named_properties[prefix]]
                     if property_path in self.headers_meta:
                         continue
-                    if type(element) not in {dict, list}:
-                        self.headers_meta[property_path] = {"properties": value_properties}
-                    elif type(element) == list:
-                        self.process_array(property_path, element)
-                    else:
-                        self.process_object(property_path, element)
+                    self.headers_meta[property_path] = {"properties": value_properties}
+                    # if type(element) not in {dict, list}:
+                    #     self.headers_meta[property_path] = {"properties": value_properties}
+                    # elif type(element) == list:
+                    #     self.process_array(property_path, element)
+                    # else:
+                    #     self.process_object(property_path, element)
             else:
                 if self.headers_meta[prefix]["count"] < len(array_value):
                     self.headers_meta[prefix]["count"] = len(array_value)
                 # Checking manually to keep properties order instead of checking subsets
-                if prefix == "offers":
-                    print("")
                 for i, element in enumerate(array_value):
                     for property_name, property_value in element.items():
                         property_path = f"{prefix}[{i}].{property_name}"
@@ -88,66 +88,78 @@ class CSVExporter:
         for field, meta in self.headers_meta.items():
             if meta.get("count") == 0:
                 continue
-            if meta.get("count") is None:
-                headers.append(field)
-                continue
-            if not meta.get("properties"):
-                for i in range(meta["count"]):
-                    headers.append(f"{field}[{i}]")
+            elif meta.get("count") is not None:
+                if not meta.get("properties"):
+                    for i in range(meta["count"]):
+                        headers.append(f"{field}[{i}]")
+                    continue
+                else:
+                    for i in range(meta["count"]):
+                        for pr in meta["properties"]:
+                            headers.append(f"{field}[{i}].{pr}")
+                    continue
             else:
-                for i in range(meta["count"]):
-                    for pr in meta["properties"]:
-                        headers.append(f"{field}[{i}].{pr}")
+                if not meta.get("properties"):
+                    headers.append(field)
+                else:
+                    for pr in meta.get("properties"):
+                        headers.append(f"{field}.{pr}")
         self.flat_headers = headers
 
     def export_product(self, product):
         row = []
+        # Using scalpl (instead of jmespath/etc.) as an existing fast backend dependency
         product_data = Cut(product)
         for header in self.flat_headers:
             header_path = header.split(".")
             if header_path[0] not in self.named_properties:
                 row.append(product_data.get(header, ""))
-            # W
+            # Assuming one nesting level of named properties
+            # like `additionalProperty.Focus Type.value`, where
+            # `name` (Focus Type) and `value` are on the same level
             else:
                 name_property = self.named_properties[header_path[0]]
+                value_found = False
                 for pr in product_data[header_path[0]]:
                     if pr.get(name_property) == header_path[1]:
-                        # TODO Require not only name of the name property, but also value property
                         row.append(pr.get(header_path[2], ""))
+                        value_found = True
+                        break
+                if not value_found:
+                    row.append("")
         return row
 
 
-with open("autocrawl/utils/csv_export_assets/products_xod_test.json") as f:
-    product_list = json.loads(f.read())
-test_named_properties = {
-    "gtin": "type",
-    "additionalProperty": "name",
-    "ratingHistogram": "ratingOption"
-}
-# TODO Pick all other properties from the objects as an additional columns
-# isbn.value, ratingHistogram.5 stars.ratingCount, ratingHistogram.5 stars.ratingPercentage etc.
-test_skip_fields = {"probability", "_key"}
+if __name__ == "__main__":
+    # Define fields where to use property values as column names
+    test_named_properties = {
+        "gtin": "type",
+        "additionalProperty": "name",
+        "ratingHistogram": "ratingOption"
+    }
+    # Define fields to provide data as-is, without additional processing
+    test_skip_fields = {"probability", "_key"}
 
-csv_exporter = CSVExporter(test_named_properties, test_skip_fields)
-for p in product_list:
-    csv_exporter.process_product(p)
+    # Load product list from JSON (simulate API response)
+    with open("autocrawl/utils/csv_export_assets/products_xod_test.json") as f:
+        product_list = json.loads(f.read())
 
-from pprint import pprint
+    csv_exporter = CSVExporter(test_named_properties, test_skip_fields)
 
-# print('*' * 500)
-# pprint(csv_exporter.headers_meta)
-# print('*' * 500)
-csv_exporter.flatten_headers()
-# pprint(csv_exporter.flat_headers)
-# print('*' * 500)
-
-import csv
-
-with open('employee_file.csv', mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    employee_writer.writerow(csv_exporter.flat_headers)
+    # Collect stats
     for p in product_list:
-        employee_writer.writerow(csv_exporter.export_product(p))
+        csv_exporter.process_product(p)
 
+    # pprint(csv_exporter.headers_meta)
 
+    # Flatten headers
+    csv_exporter.flatten_headers()
 
+    # print('*' * 500)
+    # pprint(csv_exporter.flat_headers)
+
+    with open('test_csv_export.csv', mode='w') as export_file:
+        employee_writer = csv.writer(export_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        employee_writer.writerow(csv_exporter.flat_headers)
+        for p in product_list:
+            employee_writer.writerow(csv_exporter.export_product(p))
