e4742775512a76a3b7d29c2e1f65f4cf2634df7d
sortafreel <woutut@gmail.com>
2021-07-29 02:21:12 +0300

    Add init setup and version.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import find_packages, setup
+
+NAME = "flattering"
+
+
+def get_version():
+    about = {}
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, NAME.replace("-", "_"), "__version__.py")) as f:
+        exec(f.read(), about)
+    return about["__version__"]
+
+
+setup(
+    name=NAME,
+    version=get_version(),
+    packages=find_packages(),
+    include_package_data=True,
+)
